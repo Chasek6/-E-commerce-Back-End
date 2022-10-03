eec7d79fbd1830d4959ca6e454827d03c517999f
Chasek6 <quranstratton@gmail.com>
2022-10-02 22:15:54 -0400

    readme.md

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# E-commerce
+
+# table of contents:
+
+- Description 
+- Installation
+- Tests 
+- Questions 
+
+# Description
+Created the back end for an E-Commerce to help post data, update data, and delete data.
+
+
+## Installation
+git clone & NPM I
+
+
+## Test 
+Run the following commands in your terminal to test this app:
+log into your mysql and run "Source db/schema.sql", quit and run "node seeds"  run "node server.js" and forget to "npm i"
+
+##  Questions
+
+https://github.com/Chasek6
--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,78 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- USE DATABASE
+USE ecommerce_db;
+
+-- DROP TABLES
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS product_tags;
+
+-- CREATE TABLES
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  price DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  stock INT NOT NULL DEFAULT 0,
+  category_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id)
+    REFERENCES categories(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE categories (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tags (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE product_tags (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_id INT NOT NULL,
+  tag_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id)
+    REFERENCES products(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (tag_id)
+    REFERENCES tags(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+-- INSERT DATA
+INSERT INTO categories (name)
+VALUES ('Electronics'),
+       ('Food'),
+       ('Clothing');
+
+INSERT INTO products (name, price, stock, category_id)
+
+
+
+INSERT INTO tags (name)
+VALUES ('Electronics'),
+       ('Food'),
+       ('Clothing');
+
+INSERT INTO product_tags (product_id, tag_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3);
+
+-- SELECT DATA
+SELECT * FROM products;
+SELECT * FROM categories;
+SELECT * FROM tags;
+SELECT * FROM product_tags;
